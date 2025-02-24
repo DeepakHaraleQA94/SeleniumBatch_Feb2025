cd0473c9eec1c8880290f31f452da93a10a63ef7
DeepakHaraleQA94 <haraledeepak6@gmail.com>
2025-02-24 19:20:40 +0530

    u have modified basic class file

--- src/SeleniumBacics/BasicClass.java
+++ src/SeleniumBacics/BasicClass.java
@@ -6,7 +6,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class BasicClass {
 
 	public static void main(String[] args) {
-
+//local machine2 
+		System.out.println("local machine2");
+//local machine3
+		System.out.println("local machine3");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 	}
