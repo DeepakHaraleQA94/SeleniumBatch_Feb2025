7367219af729caa37c4978819e3b6000c6ed252a
lakshmi99-qwerty <dklakshmi913@gmail.com>
2025-02-24 19:02:54 +0530

    Normalize line endings

--- src/SeleniumBacics/BasicClass.java
+++ src/SeleniumBacics/BasicClass.java
@@ -1,14 +1,38 @@
 package SeleniumBacics;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasicClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	    WebElement getLoginText = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//b[text()='JALA Academy']")));
+	    System.out.println(getLoginText.getText());
+	    
+	    
+	    
+	    WebElement radiobutton = driver.findElement(By.xpath("//input[@name='rdbGender' and @value='M']"));
+	    Thread.sleep(2000);
+	    if (!radiobutton.isSelected()) {
+	 	   radiobutton.click(); 
+	    }
+
+	    if (radiobutton.isSelected()) {
+	        System.out.println("Male radio button is selected.");
+	    } else {
+	        System.out.println("Male radio button is not selected.");
+	    }
 	}
 
 }
